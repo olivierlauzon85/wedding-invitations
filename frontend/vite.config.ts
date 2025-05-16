1ebbdbc9863549dbc5c5a198c219627d22a69674
Olivier Lauzon <olivierlauzon85@gmail.com>
2025-05-15 20:21:33 -0400

    update deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/wedding-invitations/',
   server: {
     host: "::",
     port: 8080,
